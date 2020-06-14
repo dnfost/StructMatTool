3b972f951e7297ead5b6ec3886b717cd155c5d55
dnfost <55420912+dnfost@users.noreply.github.com>
2020-06-14 17:29:11 -0400

    Strain equation corrections

--- Equations.py
+++ Equations.py
@@ -4,10 +4,19 @@ def strain(elongation, original_length, sigma, elastic_modulus):
 
         return elongation/original_length
 
-    else if(sigma and elastic_modulus):
+    elif(sigma and elastic_modulus):
 
         return sigma/elastic_modulus
+
+    elif(stress):
+
+        return stress/elastic_modulus
 
     else:
 
         print("Not enough information provided to calculate strain.")
+        return 0
+
+if __name__ == "__main__":
+
+    print(strain(2, 1000, None, None))
