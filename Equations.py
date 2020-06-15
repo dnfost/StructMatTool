83b40f9732441dd091cfd9c84bcf5c8f44d9a911
dnfost <55420912+dnfost@users.noreply.github.com>
2020-06-15 18:45:41 -0400

    Updated stress equation

--- Equations.py
+++ Equations.py
@@ -1,6 +1,10 @@
 def strain(elongation, original_length, new_length, sigma, force, area, elastic_modulus):
 
-    if(elongation and original_length):
+    if(epsilon):
+
+        return epsilon
+
+    elif(elongation and original_length):
 
         return elongation/original_length
 
@@ -29,16 +33,24 @@ def strain(elongation, original_length, new_length, sigma, force, area, elastic_
         print("Not enough information provided to calculate strain.")
         return None
 
-def stress(elongation, original_length, new_length, sigma, force, area, elastic_modulus):
+def stress(epsilon, elongation, original_length, new_length, sigma, force, area, elastic_modulus):
 
-    if (elongation and original_length and elastic_modulus):
+    if(sigma):
+
+        return sigma
+    
+    elif(elongation and original_length and elastic_modulus):
 
         return elastic_modulus * strain(elongation, original_length, None, None, None, None, None)
 
-    elif (elongation and new_length and elastic_modulus):
+    elif(elongation and new_length and elastic_modulus):
 
         return elastic_modulus * strain(elongation, None, new_length, None, None, None, None)
 
+    elif(original_length and new_length and elastic_modulus):
+
+        return elastic_modulus * strain(None, original_length, new_length, None, None, None, None)
+
 if __name__ == "__main__":
 
-    print(stress(0.02, None, 2.02, None, None, None, 1600000000))
+    print(stress(None, 2, 2.02, None, None, None, 1600000000))
